be6608ef1373be98a742e79ea6d59aeb0523f0dc
Pranav Revuri <pranavcollege7123@gmail.com>
2026-06-17 19:55:48 -0400

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0
+        right = 1
+        max_profit = 0
+
+        while right < len(prices):
+            if prices[left] > prices[right]:
+                left = right
+            profit = prices[right] - prices[left]
+            
+            if profit > max_profit:
+                max_profit = profit
+            
+            right += 1
+        
+        return max_profit
